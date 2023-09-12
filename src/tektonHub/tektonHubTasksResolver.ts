e8ab1ca14904719931d07546bef6843edff396ae
Nathan Good <nathan.good@ibm.com>
2023-09-12 15:52:57 -0700

    fix: added better support for task generation.
    Signed-off-by: Nathan Good <nathan.good@ibm.com>

--- src/tektonHub/tektonHubTasksResolver.ts
+++ src/tektonHub/tektonHubTasksResolver.ts
@@ -1,7 +1,6 @@
 import { Yaml } from 'cdk8s';
-import { ParameterBuilder, TaskBuilder, WorkspaceBuilder } from 'cdk8s-pipelines';
+import { ParameterBuilder, Task, TaskBuilder, TaskStepBuilder, WorkspaceBuilder } from 'cdk8s-pipelines';
 import { Construct } from 'constructs';
-import { TektonYaml } from './tasks';
 
 /**
  * This class handles turning a URL that points the YAML for the Tekton Hub Task into a PipelineTask
@@ -9,6 +8,7 @@ import { TektonYaml } from './tasks';
 export class TektonHubTask extends TaskBuilder {
   url: string;
   taskBuild: TaskBuilder;
+
   /**
    * Creates a new Instance of TektonHubTask with a URL that points to the Raw YAML for the task.
    * @link https://hub.tekton.dev/
@@ -21,28 +21,49 @@ export class TektonHubTask extends TaskBuilder {
     this.url = url;
     this.taskBuild = new TaskBuilder(scope, id);
   }
+
   private parseYAML(): Boolean {
-    const yamlData = this.readYamlFromUrl();
-    this.taskBuild.withName(yamlData.metadata.name);
-    const workspaces = yamlData.spec.workspaces;
+    const task = this.readYamlFromUrl();
+    this.taskBuild.withName(task.metadata?.name!);
+    const workspaces = task.spec?.workspaces;
     if (workspaces !== undefined && workspaces?.length !== 0) {
       workspaces.forEach(workspace => {
-        this.taskBuild.withWorkspace(new WorkspaceBuilder(workspace.name)
-          .withName(workspace.name)
-          .withDescription(workspace.description));
+        this.taskBuild.withWorkspace(new WorkspaceBuilder(workspace.name!)
+          .withName(workspace.name!)
+          .withDescription(workspace.description!));
       });
     }
-    const params = yamlData.spec.params;
+    const params = task.spec?.params;
     if (params !== undefined && params.length !== 0) {
       params.forEach(param => {
-        this.taskBuild.withStringParam(new ParameterBuilder(param.name)
-          .withDefaultValue(param.default)
-          .withPiplineParameter(param.name, param.default));
+        this.taskBuild.withStringParam(new ParameterBuilder(param.name!)
+          .withDescription(param.description!)
+          .withDefaultValue(param.default!)
+          .withPiplineParameter(param.name!, param.default!));
       });
     }
+
+    const steps = task.spec?.steps;
+    if (steps) {
+      steps.forEach(step => {
+        const sb =
+          new TaskStepBuilder()
+            .withName(step.name!)
+            .fromScriptData(step.script!)
+            .withWorkingDir(step.workingDir!)
+            .withArgs(step.args!)
+            .withImage(step.image!);
+        // step.env?.forEach(e => {
+        //   sb.withEnv(e.name);
+        // });
+        this.taskBuild.withStep(sb);
+      });
+    }
+
     return true;
   }
-  private readYamlFromUrl(): TektonYaml {
+
+  private readYamlFromUrl(): Task {
     try {
       // Parse the YAML content
       const parsedYaml = Yaml.load(this.url);
@@ -52,6 +73,7 @@ export class TektonHubTask extends TaskBuilder {
       throw new Error(`Error reading YAML from URL: ${errorMessage}`);
     }
   }
+
   /**
    * Returns an instance of PipelineTaskBuilder with the corresponding Tekton Hub Task Link.
    * @returns TaskBuilder
